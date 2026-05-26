7edc5b2e18150fcc57197a988003704029f5ba16
Eni Adebisi <eniadebisi@gmail.com>
2026-05-26 11:45:26 -0400

    fix DB URL

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import type { PrismaConfig } from "prisma/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 export default defineConfig({
   schema: "./prisma/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL"), 
+    url: `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT ?? 3306}/${DB_NAME}`,
   },
 }) satisfies PrismaConfig;
