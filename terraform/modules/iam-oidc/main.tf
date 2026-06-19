056efac6560e371392a9bf830310f300f76483d1
Eni Adebisi <eniadebisi@gmail.com>
2026-06-19 10:45:49 -0400

    expand gha-deploy IAM policy with EC2, EKS, RDS, IAM, AutoScaling permissions
    fck-nat module needs ec2:DescribeImages and ec2:DescribeVpcs; full
    terraform apply for the platform stack also requires EC2 create/modify
    actions (VPC, subnets, SGs, routes), EKS full access, RDS create/modify,
    IAM role creation (IRSA), and AutoScaling permissions.

--- terraform/modules/iam-oidc/main.tf
+++ terraform/modules/iam-oidc/main.tf
@@ -44,15 +44,10 @@ resource "aws_iam_role_policy" "gha_deploy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid      = "ECRAuth"
-        Effect   = "Allow"
-        Action   = ["ecr:GetAuthorizationToken"]
-        Resource = "*"
-      },
-      {
-        Sid    = "ECRRepository"
+        Sid    = "ECR"
         Effect = "Allow"
         Action = [
+          "ecr:GetAuthorizationToken",
           "ecr:BatchCheckLayerAvailability",
           "ecr:GetDownloadUrlForLayer",
           "ecr:BatchGetImage",
@@ -64,84 +59,188 @@ resource "aws_iam_role_policy" "gha_deploy" {
           "ecr:ListImages",
           "ecr:DescribeImages",
           "ecr:BatchDeleteImage",
-          "ecr:TagResource"
-        ]
-        Resource = var.ecr_repository_arn
-      },
-      {
-        Sid    = "ECS"
-        Effect = "Allow"
-        Action = [
-          "ecs:RegisterTaskDefinition",
-          "ecs:DescribeTaskDefinition",
-          "ecs:UpdateService",
-          "ecs:DescribeServices",
-          "ecs:DescribeClusters",
-          "ecs:ListTasks",
-          "ecs:DescribeTasks",
-          "ecs:TagResource"
+          "ecr:TagResource",
+          "ecr:CreateRepository",
+          "ecr:DeleteRepository",
+          "ecr:SetRepositoryPolicy",
+          "ecr:GetRepositoryPolicy",
+          "ecr:PutLifecyclePolicy"
         ]
         Resource = "*"
       },
       {
-        Sid      = "IAMPassRole"
-        Effect   = "Allow"
-        Action   = ["iam:PassRole"]
-        Resource = var.ecs_execution_role_arn
+        Sid    = "EC2"
+        Effect = "Allow"
+        Action = [
+          "ec2:Describe*",
+          "ec2:CreateTags",
+          "ec2:DeleteTags",
+          "ec2:CreateSecurityGroup",
+          "ec2:DeleteSecurityGroup",
+          "ec2:AuthorizeSecurityGroupIngress",
+          "ec2:AuthorizeSecurityGroupEgress",
+          "ec2:RevokeSecurityGroupIngress",
+          "ec2:RevokeSecurityGroupEgress",
+          "ec2:RunInstances",
+          "ec2:TerminateInstances",
+          "ec2:StopInstances",
+          "ec2:StartInstances",
+          "ec2:ModifyInstanceAttribute",
+          "ec2:CreateRoute",
+          "ec2:DeleteRoute",
+          "ec2:ReplaceRoute",
+          "ec2:CreateRouteTable",
+          "ec2:DeleteRouteTable",
+          "ec2:AssociateRouteTable",
+          "ec2:DisassociateRouteTable",
+          "ec2:CreateSubnet",
+          "ec2:DeleteSubnet",
+          "ec2:ModifySubnetAttribute",
+          "ec2:CreateVpc",
+          "ec2:DeleteVpc",
+          "ec2:ModifyVpcAttribute",
+          "ec2:CreateInternetGateway",
+          "ec2:DeleteInternetGateway",
+          "ec2:AttachInternetGateway",
+          "ec2:DetachInternetGateway",
+          "ec2:CreateNatGateway",
+          "ec2:DeleteNatGateway",
+          "ec2:AllocateAddress",
+          "ec2:ReleaseAddress",
+          "ec2:AssociateAddress",
+          "ec2:DisassociateAddress",
+          "ec2:CreateLaunchTemplate",
+          "ec2:DeleteLaunchTemplate",
+          "ec2:DescribeLaunchTemplates",
+          "ec2:DescribeLaunchTemplateVersions",
+          "ec2:CreateLaunchTemplateVersion"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "EKS"
+        Effect = "Allow"
+        Action = ["eks:*"]
+        Resource = "*"
+      },
+      {
+        Sid    = "RDS"
+        Effect = "Allow"
+        Action = [
+          "rds:Create*",
+          "rds:Delete*",
+          "rds:Describe*",
+          "rds:Modify*",
+          "rds:AddTagsToResource",
+          "rds:ListTagsForResource",
+          "rds:RestoreDBInstanceFromDBSnapshot",
+          "rds:StopDBInstance",
+          "rds:StartDBInstance"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "IAM"
+        Effect = "Allow"
+        Action = [
+          "iam:CreateRole",
+          "iam:DeleteRole",
+          "iam:GetRole",
+          "iam:ListRoles",
+          "iam:UpdateRole",
+          "iam:TagRole",
+          "iam:UntagRole",
+          "iam:PassRole",
+          "iam:AttachRolePolicy",
+          "iam:DetachRolePolicy",
+          "iam:PutRolePolicy",
+          "iam:DeleteRolePolicy",
+          "iam:GetRolePolicy",
+          "iam:ListRolePolicies",
+          "iam:ListAttachedRolePolicies",
+          "iam:CreateInstanceProfile",
+          "iam:DeleteInstanceProfile",
+          "iam:GetInstanceProfile",
+          "iam:AddRoleToInstanceProfile",
+          "iam:RemoveRoleFromInstanceProfile",
+          "iam:CreateOpenIDConnectProvider",
+          "iam:DeleteOpenIDConnectProvider",
+          "iam:GetOpenIDConnectProvider",
+          "iam:TagOpenIDConnectProvider"
+        ]
+        Resource = "*"
       },
       {
         Sid    = "SSM"
         Effect = "Allow"
         Action = [
+          "ssm:PutParameter",
+          "ssm:DeleteParameter",
           "ssm:GetParameter",
           "ssm:GetParameters",
-          "ssm:GetParametersByPath"
+          "ssm:GetParametersByPath",
+          "ssm:DescribeParameters",
+          "ssm:AddTagsToResource",
+          "ssm:ListTagsForResource"
         ]
-        Resource = "arn:aws:ssm:${var.aws_region}:${var.account_id}:parameter/${var.project_name}/*"
+        Resource = "*"
       },
       {
-        Sid      = "KMS"
-        Effect   = "Allow"
-        Action   = ["kms:Decrypt"]
-        Resource = "arn:aws:kms:${var.aws_region}:${var.account_id}:alias/aws/ssm"
+        Sid    = "KMS"
+        Effect = "Allow"
+        Action = [
+          "kms:Decrypt",
+          "kms:Encrypt",
+          "kms:GenerateDataKey",
+          "kms:DescribeKey",
+          "kms:CreateGrant"
+        ]
+        Resource = "*"
       },
       {
-        Sid    = "S3TFState"
+        Sid    = "S3"
         Effect = "Allow"
         Action = [
           "s3:GetObject",
           "s3:PutObject",
           "s3:DeleteObject",
           "s3:ListBucket",
-          "s3:GetBucketVersioning"
+          "s3:GetBucketVersioning",
+          "s3:CreateBucket",
+          "s3:DeleteBucket",
+          "s3:PutBucketPolicy",
+          "s3:GetBucketPolicy",
+          "s3:PutBucketVersioning",
+          "s3:PutBucketPublicAccessBlock",
+          "s3:GetBucketPublicAccessBlock",
+          "s3:PutEncryptionConfiguration",
+          "s3:GetEncryptionConfiguration",
+          "s3:PutBucketTagging",
+          "s3:GetBucketTagging"
         ]
-        Resource = [
-          "arn:aws:s3:::${var.tf_state_bucket}",
-          "arn:aws:s3:::${var.tf_state_bucket}/*"
-        ]
+        Resource = "*"
       },
       {
         Sid    = "CloudWatchLogs"
         Effect = "Allow"
         Action = [
           "logs:CreateLogGroup",
+          "logs:DeleteLogGroup",
           "logs:CreateLogStream",
           "logs:PutLogEvents",
           "logs:DescribeLogGroups",
-          "logs:DescribeLogStreams"
+          "logs:DescribeLogStreams",
+          "logs:PutRetentionPolicy",
+          "logs:TagResource"
         ]
-        Resource = "arn:aws:logs:${var.aws_region}:${var.account_id}:log-group:/ecs/${var.project_name}/*"
+        Resource = "*"
       },
       {
-        # Phase 4 readiness: EKS cluster access for helm deploy
-        Sid    = "EKSReadiness"
+        Sid    = "AutoScaling"
         Effect = "Allow"
         Action = [
-          "eks:DescribeCluster",
-          "eks:ListClusters",
-          "eks:AccessKubernetesApi",
-          "eks:CreateAccessEntry",
-          "eks:DescribeAccessEntry"
+          "autoscaling:*",
+          "application-autoscaling:*"
         ]
         Resource = "*"
       }
